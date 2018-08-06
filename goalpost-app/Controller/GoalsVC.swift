c07c3304034101cf427d9702e2ceea82b92d9582
Peaceman <muhammedcy@gmail.com>
2018-08-06 18:07:26 +0300

    Section 3

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -14,6 +14,7 @@ class GoalsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
