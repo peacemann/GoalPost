289d566d709766d8fd9b42fd336a855df20d93b5
Peaceman <muhammedcy@gmail.com>
2018-08-06 19:26:48 +0300

    Section 6

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -20,6 +20,8 @@ class GoalsVC: UIViewController {
     }
     
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else { return }
+        presentDetail(createGoalVC)
     }
     
     
