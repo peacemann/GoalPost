9960c45e09c9eda561978b57f87e388c142af9a5
Peaceman <muhammedcy@gmail.com>
2018-08-06 21:37:44 +0300

    Section 10

--- goalpost-app/Extensions/UIViewControllerExt.swift
+++ goalpost-app/Extensions/UIViewControllerExt.swift
@@ -28,5 +28,20 @@ extension UIViewController {
         
         dismiss(animated: false, completion: nil)
     }
+    
+    func presentSecondaryDetail(_ viewControllerToPresent: UIViewController) {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromRight
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        
+        guard let presentedViewController = presentedViewController else { return }
+        presentedViewController.dismiss(animated: false) {
+            self.view.window?.layer.add(transition, forKey: kCATransition)
+            self.present(viewControllerToPresent, animated: false, completion: nil)
+        }
+        
+    }
 }
 
